810badc167e58fd3289477f33d6d11adeb636b15
Philipp Behmer <kontakt@philippbehmer.de>
2017-04-27 12:17:21 +0200

    Use let's encrypt staging environment for non production deloyments

--- scripts/orchestration/getCertForElb.sh
+++ scripts/orchestration/getCertForElb.sh
@@ -2,7 +2,16 @@
 dsmFqdn=${1}
 curl -O https://dl.eff.org/certbot-auto
 chmod a+x certbot-auto
-./certbot-auto --debug certonly --webroot -w /usr/share/nginx/html/ -d ${dsmFqdn} --non-interactive --agree-tos --register-unsafely-without-email
+Stage=$(cat /home/ec2-user/variables/Stage)
+
+# If we are creating a production environment...
+if [ "$Stage" = "prod"]; then
+  # create an official certificate...
+  ./certbot-auto --debug certonly --webroot -w /usr/share/nginx/html/ -d ${dsmFqdn} --non-interactive --agree-tos --register-unsafely-without-email
+else
+  # else use the let's encrypt staging environment
+  ./certbot-auto --staging --debug certonly --webroot -w /usr/share/nginx/html/ -d ${dsmFqdn} --non-interactive --agree-tos --register-unsafely-without-email
+fi
 sudo chown -R ec2-user:ec2-user /etc/letsencrypt
 aws iam delete-server-certificate --server-certificate-name ${dsmFqdn}
 uploadResponse=$(aws iam upload-server-certificate --server-certificate-name ${dsmFqdn} --certificate-body file:///etc/letsencrypt/archive/${dsmFqdn}/cert1.pem --private-key file:///etc/letsencrypt/archive/${dsmFqdn}/privkey1.pem)
